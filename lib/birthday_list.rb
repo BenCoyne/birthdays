110f1a3fb088a23a471be1dd2ba8d472cdd9348c
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-12-24 15:52:57 +0000

    steps 1 and 2 completed: wrote test and passing code for add_friend and display_birthdays

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,23 @@
+class BirthdayList
 
+  def initialize
+    @birthday_list = []
+  end
+
+  def add_friend(name, birthday)
+    @birthday_list << {name: name, birthday: birthday}
+  end
+
+  def display_birthdays
+    @birthday_list.each do |friend| 
+      puts "- #{friend[:name]}: #{friend[:birthday]}"
+    end
+  end
+  
+end
+
+# list = BirthdayList.new
+# list.add_friend('Phoebe Buffay', '16 February 1967')
+# list.add_friend('Chandler Bing', '08 April 1968')
+
+# list.display_birthdays
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,29 @@
+require '/Users/bencoyne/Projects/birthdays/birthdays/lib/birthday_list.rb'
 
+describe BirthdayList do
+  subject(:bday_list) { described_class.new }
+
+  describe '#add_friend' do
+    it 'adds a friend\'s details to birthday list' do
+      expect(bday_list.add_friend('Phoebe Buffay', '16 February 1967')).to eq([{name: 'Phoebe Buffay', birthday: '16 February 1967'}])
+    end
+  end
+
+  describe '#display_birthdays' do
+    before do
+      bday_list.add_friend('Phoebe Buffay', '16 February 1967')
+      bday_list.add_friend('Chandler Bing', '08 April 1968')
+    end
+
+    it 'shows the user the stored friends, formatted on one line per friend' do
+      expect{bday_list.display_birthdays}.to output("- Phoebe Buffay: 16 February 1967\n- Chandler Bing: 08 April 1968\n").to_stdout
+    end
+  end
+end
+
+
+# Check whose birthday it is today - the program can check through 
+# the birthdays I have stored and check each one to see if it’s 
+# someone’s birthday, and tells me something like 
+# "It's Mary Poppin's birthday today! They are 124 years old!" 
+# - otherwise it won't say anything.
